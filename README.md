1498b0f7584f71a3c531fee4ecbd0498d513bc49
Braian Rosas <braianrosas@MMBA-B506.local>
2022-08-29 12:13:29 -0300

    Remove duplicated files and config webpack & laravel structure to serve bundles

---
+++ README.md
@@ -0,0 +1,4 @@
+# Laravel-Vue Skeleton
+
+- :open_file_folder: **backend** - [README](./backend/README.md)
+- :open_file_folder: **frontend** - [README](./frontend/README.md)
---
+++ backend/resources/views/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Config -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- SEO -->
+
+    <!-- Title -->
+    <title>Laravel Vue App</title>
+
+    <!-- INTEGRATIONS -->
+
+    <!-- END INTEGRATIONS -->
+
+    <!-- Stylesheets-->
+    <link href="/bundles/main.css" rel="stylesheet"></link>
+</head>
+<body>
+    <!-- App -->
+    <script src="/bundles/main.js"></script>
+</body>
+</html>
